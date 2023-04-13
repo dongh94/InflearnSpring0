ea13d4c611a73021d6f9b78b4844d727da88dd73
Donghee <dongh94@naver.com>
2023-04-13 23:57:25 +0900

    jdbc conn

--- src/main/java/com/hello/helloY/SpringConfig.java
+++ src/main/java/com/hello/helloY/SpringConfig.java
@@ -1,14 +1,27 @@
 package com.hello.helloY;
 
+import com.hello.helloY.repository.JdbcMemberRepository;
 import com.hello.helloY.repository.MemberRepository;
 import com.hello.helloY.repository.MemoryMemberRepository;
 import com.hello.helloY.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+    private DataSource dataSource;
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
+    @Bean
+    public MemberRepository memberRepository() {
+        return new JdbcMemberRepository(dataSource);
+    }
 //    @Bean
 //    public MemberRepository memberRepository() {
 //        return new MemoryMemberRepository();
